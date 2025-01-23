453ee912a06d591f2bd115cabacce44ef0fda552
aleksei_shvariov <alxsshv@gmail.com>
2025-01-23 13:06:36 +0300

    В pathResolver добавлен метод getFileSeparator, возвращающий разделитель директорий в зависимости от операционной системы. Для разделителя в ОС windows обратный слеш экранируется.

--- src/main/java/com/alxsshv/utils/PathResolver.java
+++ src/main/java/com/alxsshv/utils/PathResolver.java
@@ -11,13 +11,19 @@ import java.util.regex.Pattern;
 @Component
 @Slf4j
 public class PathResolver {
-    private final String fileSeparetor = FileSystems.getDefault().getSeparator();
+    private String fileSeparator = FileSystems.getDefault().getSeparator();
+
+
+    public String getFileSeparator(){
+        duplicateBackslashForWindowsSeparator();
+        return fileSeparator;
+    }
 
     public void createFilePathIfNotExist(String path) throws IOException {
-        String[] folders = path.split(fileSeparetor);
+        String[] folders = path.split(getFileSeparator());
         StringBuilder currentPath = new StringBuilder();
         for (String folder : folders){
-            currentPath.append(folder).append(fileSeparetor);
+            currentPath.append(folder).append(getFileSeparator());
             createFolderIfNotExist(String.valueOf(currentPath));
         }
     }
@@ -27,15 +33,26 @@ public class PathResolver {
         if (!directory.exists() && !directory.getPath().isEmpty() && isDirectory(folderPath)) {
             log.info("Создание отсутствующей директории {}", directory.getAbsolutePath());
             boolean isCreated = directory.mkdir();
-            System.out.println(directory.getAbsolutePath());
             if (!isCreated){
                 throw new IOException("Ошибка создания директории " +  directory.getPath());
             }
         }
     }
 
-    public boolean isDirectory(String fileName){
+    public boolean isDirectory(String filePath){
+        String fileName = filePath;
+        if (filePath.contains(getFileSeparator())){
+            String[] filePathParts= fileName.split(getFileSeparator());
+            fileName = filePathParts[filePathParts.length-1];
+        }
         String fileTemplate = "[\\w\\/\\\\]+\\.[\\w\\/\\\\]+";
         return !Pattern.matches(fileTemplate,fileName);
     }
+
+    private void duplicateBackslashForWindowsSeparator(){
+        if (fileSeparator.equals("\\")) {
+            fileSeparator = fileSeparator + fileSeparator;
+        }
+    }
+
 }
